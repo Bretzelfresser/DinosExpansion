4bc4290f05440dff723a55dde4c60465d582cdc4
Bretzelfresser <81638199+Bretzelfresser@users.noreply.github.com>
2021-08-27 18:25:21 +0200

    recipes

--- src/main/java/com/renatiux/dinosexpansion/datagen/server/ModRecipeProvider.java
+++ src/main/java/com/renatiux/dinosexpansion/datagen/server/ModRecipeProvider.java
@@ -2,6 +2,7 @@ package com.renatiux.dinosexpansion.datagen.server;
 
 import java.util.function.Consumer;
 
+import com.renatiux.dinosexpansion.Dinosexpansion;
 import com.renatiux.dinosexpansion.core.init.BlockInit;
 import com.renatiux.dinosexpansion.core.init.ItemInit;
 
@@ -31,11 +32,11 @@ public class ModRecipeProvider extends RecipeProvider{
 		.patternLine("xxx")
 		.addCriterion("hasItem", hasItem(Items.DIAMOND_BLOCK)).build(consumer);
 
-		CookingRecipeBuilder.smeltingRecipe(Ingredient.fromItems(ItemInit.RAW_BROWNSTONE.get()), ItemInit.POLISHED_BROWNSTONE.get(), 1.0F, 200).addCriterion("has_raw_brownstone", hasItem(ItemInit.RAW_BROWNSTONE.get())).build(consumer, "polished_brownstone_from_smelting");
-		CookingRecipeBuilder.blastingRecipe(Ingredient.fromItems(ItemInit.RAW_BROWNSTONE.get()), ItemInit.POLISHED_BROWNSTONE.get(), 1.0F, 100).addCriterion("has_raw_brownstone", hasItem(ItemInit.RAW_BROWNSTONE.get())).build(consumer, "polished_brownstone_from_blasting");
+		CookingRecipeBuilder.smeltingRecipe(Ingredient.fromItems(ItemInit.RAW_BROWNSTONE.get()), ItemInit.POLISHED_BROWNSTONE.get(), 1.0F, 200).addCriterion("has_raw_brownstone", hasItem(ItemInit.RAW_BROWNSTONE.get())).build(consumer, Dinosexpansion.modLoc("polished_brownstone_from_smelting"));
+		CookingRecipeBuilder.blastingRecipe(Ingredient.fromItems(ItemInit.RAW_BROWNSTONE.get()), ItemInit.POLISHED_BROWNSTONE.get(), 1.0F, 100).addCriterion("has_raw_brownstone", hasItem(ItemInit.RAW_BROWNSTONE.get())).build(consumer, Dinosexpansion.modLoc("polished_brownstone_from_blasting"));
 
-		CookingRecipeBuilder.smeltingRecipe(Ingredient.fromItems(BlockInit.DINO_PURPLE_GEN_ORE.get()), ItemInit.PURPLE_GEM.get(), 1.0F, 200).addCriterion("has_purple_gen_ore", hasItem(BlockInit.DINO_PURPLE_GEN_ORE.get())).build(consumer, "purple_gem_from_smelting");
-		CookingRecipeBuilder.blastingRecipe(Ingredient.fromItems(BlockInit.DINO_PURPLE_GEN_ORE.get()), ItemInit.PURPLE_GEM.get(), 1.0F, 100).addCriterion("has_purple_gen_ore", hasItem(BlockInit.DINO_PURPLE_GEN_ORE.get())).build(consumer, "purple_gem_from_blasting");
+		CookingRecipeBuilder.smeltingRecipe(Ingredient.fromItems(BlockInit.DINO_PURPLE_GEN_ORE.get()), ItemInit.PURPLE_GEM.get(), 1.0F, 200).addCriterion("has_purple_gen_ore", hasItem(BlockInit.DINO_PURPLE_GEN_ORE.get())).build(consumer, Dinosexpansion.modLoc("purple_gem_from_smelting"));
+		CookingRecipeBuilder.blastingRecipe(Ingredient.fromItems(BlockInit.DINO_PURPLE_GEN_ORE.get()), ItemInit.PURPLE_GEM.get(), 1.0F, 100).addCriterion("has_purple_gen_ore", hasItem(BlockInit.DINO_PURPLE_GEN_ORE.get())).build(consumer, Dinosexpansion.modLoc("purple_gem_from_blasting"));
 
 	}
 }
