fa72c45eb64483517ac470cc5a6906db4773b548
RenaatiuX <77760642+RenaatiuX@users.noreply.github.com>
2021-10-01 01:24:45 -0500

    biome dont work correctly

--- src/main/java/com/renatiux/dinosexpansion/world/dimension/layers/DinoRiverLayer.java
+++ src/main/java/com/renatiux/dinosexpansion/world/dimension/layers/DinoRiverLayer.java
@@ -5,9 +5,11 @@ import com.renatiux.dinosexpansion.world.dimension.dino.DinoLayerUtil;
 import net.minecraft.world.gen.INoiseRandom;
 import net.minecraft.world.gen.layer.traits.ICastleTransformer;
 
-public class DinoRiverLayer implements ICastleTransformer {
+public enum DinoRiverLayer implements ICastleTransformer {
 
-    public DinoRiverLayer() { }
+    INSTANCE;
+
+    DinoRiverLayer() { }
 
     @Override
     public int apply(INoiseRandom random, int north, int east, int south, int west, int center)
@@ -18,5 +20,19 @@ public class DinoRiverLayer implements ICastleTransformer {
         }
 
         return -1;
+    }
+
+    boolean shouldRiver(int mid, int left, int down, int right, int up)
+    {
+        return shouldRiver(mid, left) || shouldRiver(mid, right) || shouldRiver(mid, down) || shouldRiver(mid, up);
+    }
+
+    boolean shouldRiver(int id1, int id2)
+    {
+        if(id1 == id2)
+            return false;
+        if(id1 == id2)
+            return false;
+        return true;
     }
 }
