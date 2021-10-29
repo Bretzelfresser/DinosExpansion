a71db6f947bde80fa7d4ae44a93732b49a02f45f
RenaatiuX <77760642+RenaatiuX@users.noreply.github.com>
2021-10-29 02:26:14 -0500

    nothing

---
+++ src/main/java/com/renatiux/dinosexpansion/common/items/WormItem.java
@@ -0,0 +1,17 @@
+package com.renatiux.dinosexpansion.common.items;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.util.ActionResultType;
+
+public class WormItem extends Item {
+
+    public WormItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public ActionResultType onItemUse(ItemUseContext context) {
+        return super.onItemUse(context);
+    }
+}
